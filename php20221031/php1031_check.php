1eaf4aa433da258a74286a3fcaea21690317d036
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-10-31 13:17:47 +0800

    新增會員登入-session版登入檢查頁面

---
+++ php20221031/php1031_check.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+$acc = 'lulu';
+$pw = '1234';
+
+$formAcc = $_POST['acc'];
+$formPw = $_POST['pw'];
+
+if ($acc == $formAcc && $pw == $formPw) {
+    // echo "帳密正確";
+    $_SESSION['login']= $formAcc;
+} 
+    // echo "帳密錯誤";
+    
+    header("location:php1031_login.php");
+
