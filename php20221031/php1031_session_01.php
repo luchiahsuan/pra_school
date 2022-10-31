c81394a23b1d352303a9e55bb7dda2567bb92ecf
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-10-31 11:01:16 +0800

    session簡易連結

---
+++ php20221031/php1031_session_01.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>session</title>
+    </head>
+    <body>
+    <h1>session練習</h1>
+    <?php
+    
+    session_start();
+    $_SESSION['name']='LULU';
+
+    echo $_SESSION['name'];
+    
+    ?>
+    <a href="./php1031_session_02.php">會員中心</a>
+    
+</body>
+</html>
