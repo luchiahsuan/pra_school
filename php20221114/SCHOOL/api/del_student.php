39f85d833bb6b6439144d5655c56ca22b799e803
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-11-21 14:36:44 +0800

    美化網頁,完整刪除系統,新增教師註冊,更改檔案名稱

---
+++ php20221114/SCHOOL/api/del_student.php
@@ -0,0 +1,24 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=school";
+$pdo=new PDO($dsn,'root','');
+
+//echo $_GET['id'];
+$student=$pdo->query("SELECT * FROM `students` where `id`='{$_GET['id']}'")
+             ->fetch(PDO::FETCH_ASSOC);
+$sql_class="DELETE FROM `class_student` WHERE `school_num`='{$student['school_num']}'";
+$sql_student="DELETE FROM `students` WHERE `id`='{$_GET['id']}'";
+
+echo $sql_class;
+echo "<br>";
+echo $sql_student;
+echo "<br>";
+//$pdo->query($sql);
+$res_class=$pdo->exec($sql_class);
+$res_student=$pdo->exec($sql_student);
+echo $res_class;
+echo "<br>";
+echo $res_student;
+echo "刪除成功:";
+
+header("location:../index.php?del=已成功刪除學生{$student['name']}的所有資料！！");
+?>
--- php20221114/SCHOOL/del.php
+++ php20221114/SCHOOL/del.php
@@ -7,6 +7,6 @@
     <title>移除學生</title>
 </head>
 <body>
-    <h1>移除學生</h1>
+<h1>移除學生</h1>
 </body>
 </html>
