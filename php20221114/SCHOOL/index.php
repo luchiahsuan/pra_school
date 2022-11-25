ed20edbd28d694032cc9b32a380d23aa8815bb62
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-11-25 10:58:38 +0800

    班級nav拆出用inclued連結

--- php20221114/SCHOOL/index.php
+++ php20221114/SCHOOL/index.php
@@ -69,18 +69,9 @@ $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     <a href="reg.php">教師註冊</a>
     <a href="login.php">教師登入</a>
 </nav>
-<nav>
-<ul class="class-list">
 <?php
-    //從`classes`資料表中撈出所有的班級資料並在網頁上製作成下拉選單的項目
-    $sql="SELECT `id`,`code`,`name` FROM `classes`";
-    $classes=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-    foreach($classes as $class){
-        echo "<li><a href='?code={$class['code']}'>{$class['name']}</a></li>";
-    }
-?>  
-</ul>
-</nav>
+    include"./layouts/class_nav.php";
+?>
 <!--根據status來顯示回應-->
 <?php
 if(isset($_GET['status'])){
