599d77b1f1d3f8a1e57b836001564cca2fc72dc2
chiahsuanlu <--lu2010_81509@yahoo.com.tw>
2022-12-01 11:40:09 +0800

    menu練習修改與JS陣列前導

---
+++ web20221201/array_pra_01.php
@@ -0,0 +1,15 @@
+<?php
+$mobileArr = array(
+    "apple"=>array("black","64g","24800"),
+    "htc"=>array("purple","64g","22500"),
+    "lg"=>array("pink","64g","26700"),
+    "samsung"=>array("white","64g","28900")
+
+);
+echo $mobileArr["lg"][1][3];
+
+$phoneArr=[
+
+]
+
+?>
