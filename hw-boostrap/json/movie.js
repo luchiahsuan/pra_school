09a8065f630c2993951b71547b2196c78e808b88
chiahsuanlu <lu2010_81509@yahoo.com.tw>
2023-01-24 22:16:31 +0800

    建立電影資料json檔,角色json檔移至資料夾

---
+++ hw-boostrap/json/movie.js
@@ -0,0 +1,76 @@
+{
+    [
+        {
+            "id": 1,
+            "book": "哈利波特：神秘的魔法石",
+            "original_title": "Harry Potter and the Sorcerer's Stone",
+            "release_date": "2001年11月16日",
+            "author": "J. K. Rowling",
+            "director": "克里斯·哥倫布",
+            "description": "一個看似普通11歲男孩的哈利波特，實際上他是個巫師，也是佛地魔試圖崛起的倖存者。哈利被魯霸·海格從他不友善的麻瓜親戚—弗農姨丈、姨姨佩妮和他的表弟達力手中救出，並送到霍格華茲魔法與巫術學院就讀，哈利於那裡跟他的朋友榮恩·衛斯理和妙麗·格蘭傑陷入了被保存在學校裡的魔法石的謎團之中。"
+        },
+        {
+            "id": 2,
+            "book": "哈利波特：消失的密室",
+            "original_title": "Harry Potter and the chamber of secrets",
+            "release_date": "2002年11月15日",
+            "author": "J. K. Rowling",
+            "director": "克里斯·哥倫布",
+            "description": "哈利、榮恩和妙麗等人回到霍格華茲繼續他們的二年級的學業，但隱藏於學校裡的一個消失的密室被打開，讓學生和鬼魂被一名身份不明的東西嚇呆了。三人必須解開密室之謎，並找到密室的入口，才能找到並擊敗真正的罪魁禍首。"
+        },
+        {
+            "id": 3,
+            "book": "哈利波特：阿茲卡班的逃犯",
+            "original_title": "Harry Potter and the Prisoner of Azkaban",
+            "release_date": "2004年5月31日",
+            "author": "J. K. Rowling",
+            "director": "艾方索·柯朗",
+            "description": "一個神秘的罪犯天狼星·布萊克從阿茲卡班監獄逃脫，並把其目光投向霍格華茲—催狂魔駐紮在那裡以保護哈利與他的同學。這讓哈利更了解自己的過去以及自己跟這個逃犯的關聯。"
+        },
+        {
+            "id": 4,
+            "book": "哈利波特：火盃的考驗",
+            "original_title": "Harry Potter and the Goblet of Fire",
+            "release_date": "2005年11月18日",
+            "author": "J. K. Rowling",
+            "director": "麥克·紐威爾",
+            "description": "魁地奇世界盃結束後，哈利回到霍格華茲並發現自己參加了三巫鬥法大賽，那是一項涉及完成三項具挑戰性危險任務的比賽。哈利被迫跟火盃選擇的另外三名巫師進行競賽—花兒·戴樂古、維克多·喀浪和西追·迪哥里。"
+        },
+        {
+            "id": 5,
+            "book": "哈利波特：鳳凰會的密令",
+            "original_title": "Harry Potter and the Order of the Phoenix",
+            "release_date": "2007年7月11日",
+            "author": "J. K. Rowling",
+            "director": "大衛·葉慈",
+            "description": "哈利回到霍格華茲繼續其第五年的學業，並發現魔法世界否認佛地魔捲土重來的事實。他把事情掌握於自己手中並成立了一個秘密組織來對抗霍格華茲「高級調查官」桃樂絲·恩不里居的政權，以及為即將到來的戰鬥學習實用性的黑魔法防禦術。"
+        },
+        {
+            "id": 6,
+            "book": "哈利波特：混血王子的背叛",
+            "original_title": "Harry Potter and the Half-Blood Prince",
+            "release_date": "2009年7月15日",
+            "author": "J. K. Rowling",
+            "director": "大衛·葉慈",
+            "description": "佛地魔和其食死人正在增加他們對巫師與麻瓜世界的恐懼。鄧不利多校長出於重要原因需要幫助，他說服其老朋友赫瑞司·史拉轟回到其於霍格華茲先前的職位。在史拉轟的魔藥課堂上，哈利得到了一本帶有奇怪註釋的教科書，這課本之前為「混血王子」所有。"
+        },
+        {
+            "id": 7,
+            "book": "哈利波特：死神的聖物Ⅰ",
+            "original_title": "Harry Potter and the Deathly Hallows",
+            "release_date": "2010年11月19日",
+            "author": "J. K. Rowling",
+            "director": "大衛·葉慈",
+            "description": "哈利、榮恩和妙麗離開了霍格華茲，並踏上尋找並摧毀佛地魔永生的秘密——分靈體之旅。這三人組經歷了漫長的旅程，他們在路途上遇到了許多障礙，包括食死人、掠奪者、神秘的死神聖物，以及哈利與黑魔王思想的連接變得越來越強。"
+        },
+        {
+            "id": 8,
+            "book": "哈利波特：死神的聖物ⅠⅠ",
+            "original_title": "Harry Potter and the Cursed Child",
+            "release_date": "2011年7月15日",
+            "author": "J. K. Rowling",
+            "director": "無電影化",
+            "description": "當哈利準備跟佛地魔的最後一戰時，哈利、榮恩和妙麗同時繼續尋找並摧毀餘下的分靈體。"
+        }
+    ]
+}
